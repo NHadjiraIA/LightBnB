c48f002b5a20e2fe37bffbdfe374c293462699bc
Hadjira <bkh.hadjira@gmail.com>
2021-07-26 23:21:48 +0000

    LighthouseBnB  Reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -74,7 +74,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT *
+  FROM reservations
+  WHERE guest_id = $1
+  LIMIT $2;
+  `;
+  return pool
+  .query(queryString,[guest_id,limit])
+  .then(res => console.log(res.rows[0]))
+  .catch(err => console.log(err.message));
 }
 exports.getAllReservations = getAllReservations;
 
